5c53cfd725f40ec398be6c3102458cca58b8eb28
usmc-Blockhead <jaysplaypen@gmail.com>
2023-01-07 13:29:26 -0800

    workshop1

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from 'react';
 import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
+import CampsitesList from './features/campsites/CampsitesList.js';
 
 function App() {
     return (
         <div className='App'>
-            <Navbar dark color='primary' sticky='top' expand='md'>
-                <Container>
-                    <NavbarBrand href='/'>
-                        <img src={NucampLogo} alt='nucamp logo' />
-                    </NavbarBrand>
-                </Container>
-            </Navbar>
-            I'm ready for workshop!
+            <Header/>
+            <CampsitesList/>
+            <Footer/>
         </div>
     );
 }
