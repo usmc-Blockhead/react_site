77c7ac6ed85a34fcd08f36f0918db69aabbe21e9
usmc-Blockhead <jaysplaypen@gmail.com>
2023-01-13 17:30:58 -0800

    CampsitesDirectoryPage and CampsiteDetail

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import CampsitesList from './features/campsites/CampsitesList.js';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
@@ -8,7 +8,7 @@ function App() {
     return (
         <div className='App'>
             <Header/>
-            <CampsitesList/>
+            <CampsitesDirectoryPage />
             <Footer/>
         </div>
     );
---
+++ src/features/campsites/CampsiteDetail.js
@@ -0,0 +1,18 @@
+import { Card, Col, CardImg, CardText, CardBody } from 'reactstrap';
+
+const CampsiteDetail = ( {campsite} ) => {
+    const { image, name, description } = campsite;
+
+    return (
+        <Col md='12' className='m-4'>
+            <Card>
+                <CardImg top src={image} alt={name} />
+                <CardBody>
+                    <CardText>{description}</CardText>
+                </CardBody>
+            </Card>
+        </Col>
+    );
+};
+
+export default CampsiteDetail;
